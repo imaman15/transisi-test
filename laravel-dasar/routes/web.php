bc7587a0bdf9b3f0b6c6d8ec724ab143244c0f83
imaman15 <imamagustiannugraha@ymail.com>
2021-09-30 19:32:46 +0700

    Add laravel UI Bootstrap

--- laravel-dasar/routes/web.php
+++ laravel-dasar/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['reset' => false, 'register' => false, 'email' => false, 'request' => false, 'udpate' => false, 'confirm' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
