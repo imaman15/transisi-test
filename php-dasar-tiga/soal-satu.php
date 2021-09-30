ae3b4df761dc88b2f7ff04a2cae0fc0edf08997f
imaman15 <imamagustiannugraha@ymail.com>
2021-09-30 10:48:17 +0700

    php dasar 1 selesai

---
+++ php-dasar-tiga/soal-satu.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = [
+  ['f', 'g', 'h', 'i'],
+  ['j', 'k', 'p', 'q'],
+  ['r', 's', 't', 'u']
+];
+
+function cari($arr, $input) {
+  $inputArr = explode(" ", $input);
+}
+
+echo cari($arr, 'fghi') . "<br>";
